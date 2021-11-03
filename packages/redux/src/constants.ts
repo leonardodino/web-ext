2f00bfa1fc386c799167e0b261ac06a13a66c979
Oleg Obidin <obidin.oleg@yandex.ru>
2021-11-03 15:33:56 +0000

    :sparkles:  add support for Safari on iOS (#5)
    Co-authored-by: Oleg Obidin <oleg.obidin@revolut.com>

--- packages/redux/src/constants.ts
+++ packages/redux/src/constants.ts
@@ -1,7 +1,7 @@
-export const reply = '@web-ext/redux – reply'
-export const thunk = '@web-ext/redux – thunk'
-export const dispatch = '@web-ext/redux – dispatch'
-export const newState = '@web-ext/redux – newState'
+export const reply = '@web-ext/redux/reply'
+export const thunk = '@web-ext/redux/thunk'
+export const dispatch = '@web-ext/redux/dispatch'
+export const newState = '@web-ext/redux/newState'
 
 /** internal symbol to denote store connection state */
 export const connected: unique symbol = Symbol('connected')
